af5b0df0d13b321db76444655208b841da40ea4e
Kehinde Adeoso <kennolite@gmail.com>
2024-02-06 01:05:28 -0500

    initial commit

---
+++ star-maker.py
@@ -0,0 +1,47 @@
+# Kehinde Adeoso
+# kehinde.adeoso77@myhunter.cuny.edu
+# Star-Maker
+
+import turtle
+t = turtle.Turtle()
+x = -350
+y = 300
+
+bx, by = 350, 300
+
+t.penup()
+t.goto(x, y)
+
+def position_check():
+    x = -350
+    y = 300
+    if t.xcor() > bx:
+        t.penup()
+        y -= 110
+        t.goto(-350, y)
+        t.pendown()
+
+
+def create_star():
+    position_check()
+    t.speed(10)
+    t.pendown()
+    for i in range(5):
+        t.forward(100)
+        t.right(144)
+    t.penup()
+    t.forward(150)
+    
+
+for i in range(12):
+    message = 'Stars Created!'
+    create_star()
+    if i + 1 == 1:
+        message = 'Star Created'
+    else:
+        message = 'Stars Created'
+        
+    print((i + 1), message)
+t.hideturtle()
+input("Mission Complete! Click here and press ENTER ") # keeps turtles from closing prematurely
+
