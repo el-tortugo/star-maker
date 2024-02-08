c113e8751e88864ce16612a29f5a264a3b9b4586
Kehinde Adeoso <kennolite@gmail.com>
2024-02-07 22:48:41 -0500

    fixed position_check(), added custom amount function

--- star-maker.py
+++ star-maker.py
@@ -4,15 +4,19 @@
 
 import turtle
 t = turtle.Turtle()
+wn = turtle.Screen()
 x = -350
 y = 300
+
+n = input('How many stars? ')
 
 bx, by = 350, 300
 
 t.penup()
 t.goto(x, y)
 
-def position_check(x, y):
+def position_check():
+    global x, y
     if t.xcor() > bx:
         t.penup()
         y -= 110
@@ -20,8 +24,8 @@ def position_check(x, y):
     t.pendown()
 
 
-def create_star(x, y):
-    position_check(x, y)
+def create_star():
+    position_check()
     t.speed(10)
     t.pendown()
     for i in range(5):
@@ -31,8 +35,8 @@ def create_star(x, y):
     t.forward(150)
     
 
-for i in range(12):
-    create_star(x, y)
+for i in range(int(n)):
+    create_star()
     if i + 1 == 1:
         message = 'Star Created'
     else:
@@ -40,7 +44,4 @@ for i in range(12):
         
     print((i + 1), message)
 t.hideturtle()
-input("Mission Complete! Click here and press ENTER ") # keeps turtles from closing prematurely
-
-
-
+wn.exitonclick()
